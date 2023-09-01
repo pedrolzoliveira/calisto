f45141ca67c9eb769bf19585deb9813980fa6f71
Pedro Oliveira <pluiz@mychirotouch.com>
2023-09-01 01:34:37 -0300

    refactor(get-html): add logger

--- src/application/news/utils/get-html.ts
+++ src/application/news/utils/get-html.ts
@@ -1,9 +1,23 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import { parse } from 'node-html-parser'
+import { logger } from '@/src/infra/logger'
 
 export const getHTML = async (link: string) => {
-  const response = await axios.get(link)
-  const html = parse(response.data)
-  html.querySelectorAll('script').forEach(script => script.remove())
-  return html
+  const response = await axios.get(link).catch(error => {
+    if (error instanceof AxiosError) {
+      logger.error({ error, link, response: error.response?.data })
+    } else {
+      logger.error({ error, link })
+    }
+    throw error
+  })
+
+  try {
+    const html = parse(response.data)
+    html.querySelectorAll('script').forEach(script => script.remove())
+    return html
+  } catch (error) {
+    logger.error({ error, link, response: response.data })
+    throw error
+  }
 }
