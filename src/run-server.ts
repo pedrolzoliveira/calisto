377ce128c3171e96d47dc09244cbced2344b63f3
Pedro Oliveira <pluiz@mychirotouch.com>
2023-09-01 00:36:27 -0300

    better import

--- src/run-server.ts
+++ src/run-server.ts
@@ -1,6 +1,6 @@
 import { server } from '@/src/infra/http/server'
 import { env } from '@/src/config/env'
-import { logger } from './infra/logger'
+import { logger } from '@/src/infra/logger'
 
 server.listen(env.PORT, () => {
   logger.info(`listening on port ${env.PORT}`)
