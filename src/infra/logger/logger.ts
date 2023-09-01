285d64a8549bdaa73adc0e361a830fd42c760cc0
Pedro Oliveira <pluiz@mychirotouch.com>
2023-08-31 21:21:11 -0300

    better logging

--- src/infra/logger/logger.ts
+++ src/infra/logger/logger.ts
@@ -1,8 +1,12 @@
 import winston from 'winston'
 
+const { combine, timestamp, prettyPrint } = winston.format
+
+const format = combine(timestamp(), prettyPrint())
+
 export const logger = winston.createLogger({
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: 'logs/combined.log' })
+    new winston.transports.Console({ format }),
+    new winston.transports.File({ format, filename: 'logs/combined.log', level: 'error' })
   ]
 })
