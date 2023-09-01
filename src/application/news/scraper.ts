20859b51c186af21cf48283f23d981ffc560e36d
Pedro Oliveira <pluiz@mychirotouch.com>
2023-09-01 18:55:39 -0300

    use logger.error in scraper

--- src/application/news/scraper.ts
+++ src/application/news/scraper.ts
@@ -4,9 +4,10 @@ import { scrapeFolhaNews } from './sources/folha/jobs/scrape-news'
 import { scrapeValorNews } from './sources/valor/jobs/scrape-news'
 import { scrapeUolNews } from './sources/uol/jobs/scrape-news'
 import { scrapeG1News } from './sources/g1/jobs/scrape-news'
+import { logger } from '@/src/infra/logger'
 
-schedule('* * * * *', async () => { await scrapeEstadaoNews().catch(console.error) }).start()
-schedule('* * * * *', async () => { await scrapeFolhaNews().catch(console.error) }).start()
-schedule('* * * * *', async () => { await scrapeValorNews().catch(console.error) }).start()
-schedule('* * * * *', async () => { await scrapeUolNews().catch(console.error) }).start()
-schedule('* * * * *', async () => { await scrapeG1News().catch(console.error) }).start()
+schedule('* * * * *', async () => { await scrapeEstadaoNews().catch(logger.error) }).start()
+schedule('* * * * *', async () => { await scrapeFolhaNews().catch(logger.error) }).start()
+schedule('* * * * *', async () => { await scrapeValorNews().catch(logger.error) }).start()
+schedule('* * * * *', async () => { await scrapeUolNews().catch(logger.error) }).start()
+schedule('* * * * *', async () => { await scrapeG1News().catch(logger.error) }).start()
