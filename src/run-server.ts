72587bc4090ee1b65974865ce83f58548232ff83
Pedro Oliveira <48186206+pedrolzoliveira@users.noreply.github.com>
2023-09-04 00:26:55 +0000

    change import order

--- src/run-server.ts
+++ src/run-server.ts
@@ -1,6 +1,6 @@
 import { server } from '@/src/infra/http/server'
-import { env } from '@/src/config/env'
 import { logger } from '@/src/infra/logger'
+import { env } from '@/src/config/env'
 
 server.listen(env.PORT, () => {
   logger.info(`listening on port ${env.PORT}`)
