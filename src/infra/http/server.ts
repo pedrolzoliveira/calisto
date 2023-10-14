85655a2c82e12beb2392ac97d18290529c811fe9
Pedro Oliveira <pluiz@mychirotouch.com>
2023-10-14 19:28:33 -0300

    add templating stuff

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,19 +1,29 @@
 import 'express-async-errors'
 
-import express, { json } from 'express'
-import cors from 'cors'
+import express from 'express'
+import { join } from 'path'
+import { viewController } from './www/controllers/views-controller'
+// import express, { json } from 'express'
+// import cors from 'cors'
 
-import { env } from '@/src/config/env'
-import { newsController } from '@/src/application/news/controller'
-import { profilesController } from '@/src/application/profiles/controller'
-import { errorHandler } from './error-handler'
+// import { env } from '@/src/config/env'
+// import { newsController } from '@/src/application/news/controller'
+// import { profilesController } from '@/src/application/profiles/controller'
+// import { errorHandler } from './error-handler'
 
 export const server = express()
 
-server.use(json())
-server.use(cors({ origin: env.CORS_ORIGIN }))
+server.set('view engine', 'ejs')
+server.set('views', join(__dirname, 'www', 'views'))
 
-server.use('/profiles', profilesController)
-server.use('/news', newsController)
+server.use('/dist', express.static(join(__dirname, 'www', 'dist')))
 
-server.use(errorHandler)
+server.use('/', viewController)
+
+// server.use(json())
+// server.use(cors({ origin: env.CORS_ORIGIN }))
+
+// server.use('/profiles', profilesController)
+// server.use('/news', newsController)
+
+// server.use(errorHandler)
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/infra/http/www/views/**/*.ejs'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
