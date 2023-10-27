0482ce3c43d08cf5e395f1c4bd195abcfa7fa879
Pedro Oliveira <pluiz@mychirotouch.com>
2023-10-27 14:01:43 -0300

    add @lit-labs/ssr

--- src/application/news/controller.ts
+++ src/application/news/controller.ts
@@ -2,6 +2,10 @@ import { Router } from 'express'
 import { z } from 'zod'
 import { getNewsFeed } from './queries/get-news-feed'
 import { prismaClient } from '@/src/infra/database/prisma/client'
+import { render } from '@lit-labs/ssr'
+import { newsCard } from '@/src/infra/http/www/views/partials/news-card'
+import { RenderResultReadable } from '@lit-labs/ssr/lib/render-result-readable.js'
+
 export const newsController = Router()
 
 newsController.get('/',
@@ -45,4 +49,24 @@ newsController.get('/feed', async (req, res) => {
 
   res.setHeader('HX-Push-Url', `/news?profileId=${data.profileId}`)
   return res.render('partials/news-feed', { news, profileId: data.profileId, layout: false })
+})
+
+newsController.get('/testing-lit', async (req, res) => {
+  const [news] = await getNewsFeed({
+    profileId: 'b6394d0f-e3d0-4b3d-8154-b4ae2c54679d',
+    limit: 1,
+    cursor: new Date()
+  })
+
+  const result = render(newsCard(news))
+
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
+  res.setHeader('Transfer-Encoding', 'chunked')
+
+  const readableResult = new RenderResultReadable(result)
+  // res.write(readableResult.read())
+
+  res.send(readableResult.read())
+
+  // res.write()
 })
