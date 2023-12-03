31e2c8782dd9f560855feb746a2e2dc1ed0e4b00
Pedro Oliveira <pluiz@mychirotouch.com>
2023-12-03 02:26:34 -0300

    Refactor news-created.test.ts

--- src/application/news/consumers/news-created.test.ts
+++ src/application/news/consumers/news-created.test.ts
@@ -1,9 +1,8 @@
-import { after, before, test } from 'node:test'
-import { processingRelationsQueue } from '@/src/application/chat-gpt/queues/processing-relations'
+import { after, before, describe, it, test } from 'node:test'
 import { newsCreatedQueue } from '../queues/news-created'
 import assert from 'node:assert'
 import { newsFactory } from '@/src/test-utils/factories/news-factory'
-import { stub } from 'sinon'
+import { type SinonStub, stub } from 'sinon'
 import { newsCreatedConsumer } from '../consumers/news-created'
 import { profileFactory } from '@/src/test-utils/factories/profile-factory'
 import { prismaClient } from '@/src/infra/database/prisma/client'
@@ -11,6 +10,8 @@ import { type Queue } from '@/src/infra/messaging/rabbitmq/queue'
 import { publisher } from '../../publisher'
 import { createConnection } from '@/src/infra/messaging/rabbitmq/create-connection'
 import { createChannel } from '@/src/infra/messaging/rabbitmq/create-channel'
+import { type Channel, type Connection } from 'amqplib'
+import { type NewsCategory } from '@prisma/client'
 
 const sleep = async (ms: number) => await new Promise(resolve => setTimeout(resolve, ms))
 
@@ -21,36 +22,42 @@ const waitForQueue = async (queue: Queue): Promise<any> => {
   }
 }
 
-test('news-created consumer', async (t) => {
-  const testConnection = await createConnection()
-  const testChannel = await createChannel(testConnection)
+describe('news-created consumer', async () => {
+  let testConnection: Connection
+  let testChannel: Channel
+  let publisherStub: SinonStub
+  let link: string
+  let categories: string[]
+  let newsCategories: Set<NewsCategory>
+  let expectedNewsCategories: Set<NewsCategory>
 
-  await testChannel.purgeQueue('news-created')
+  before(async () => {
+    testConnection = await createConnection()
+    testChannel = await createChannel(testConnection)
 
-  newsCreatedQueue.bindChannel(testChannel)
-  newsCreatedConsumer.bindChannel(testChannel)
+    await testChannel.purgeQueue('news-created')
 
-  const publisherStub = stub(publisher, 'publish')
+    newsCreatedQueue.bindChannel(testChannel)
+    newsCreatedConsumer.bindChannel(testChannel)
 
-  const { link } = await newsFactory.create()
-  const { categories } = await profileFactory.create()
+    publisherStub = stub(publisher, 'publish')
 
-  const testPublisher = newsCreatedQueue.createPublisher()
+    const news = await newsFactory.create()
+    const profile = await profileFactory.create()
 
-  testPublisher.publish('news-created', { link })
+    link = news.link
+    categories = profile.categories
 
-  await newsCreatedConsumer.consume()
+    const testPublisher = newsCreatedQueue.createPublisher()
 
-  await waitForQueue(newsCreatedQueue)
+    testPublisher.publish('news-created', { link })
 
-  await newsCreatedConsumer.stop()
+    await newsCreatedConsumer.consume()
 
-  await testChannel.close()
-  await testConnection.close()
+    await waitForQueue(newsCreatedQueue)
 
-  await t.test('creates newsCategories for each category for that news', async (t) => {
-    const newsCategories = new Set(await prismaClient.newsCategory.findMany())
-    const expectedNewsCategories = new Set(
+    newsCategories = new Set(await prismaClient.newsCategory.findMany())
+    expectedNewsCategories = new Set(
       categories.map(category => ({
         batchId: null,
         newsLink: link,
@@ -59,11 +66,19 @@ test('news-created consumer', async (t) => {
         processed: false
       }))
     )
+  })
 
+  after(async () => {
+    await newsCreatedConsumer.stop()
+    await testChannel.close()
+    await testConnection.close()
+  })
+
+  it('creates newsCategories for each category for that news', () => {
     assert.deepStrictEqual(newsCategories, expectedNewsCategories)
   })
 
-  await t.test('sends a message to processing-relations queue', (t) => {
+  it('sends a message to processing-relations queue', () => {
     assert.deepEqual(publisherStub.getCall(0).args[0], 'processing-relations')
     assert.deepEqual(publisherStub.getCall(0).args[1], { link })
   })
