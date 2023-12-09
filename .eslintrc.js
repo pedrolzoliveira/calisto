9e548d6c43d0b622ca69bd5c0de429c69e596c3c
Pedro Oliveira <pluiz@mychirotouch.com>
2023-12-09 10:59:09 -0300

    Disable TypeScript eslint rule for semi-colons

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
     '@typescript-eslint/no-confusing-void-expression': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     semi: 'off',
+    '@typescript-eslint/semi': 'off',
     '@stylistic/js/semi': ['error', 'always']
   }
 };
