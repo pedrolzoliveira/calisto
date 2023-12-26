b7dea8424d3d0d5f3abb891e75197d94dd9904c5
Pedro Oliveira <pluiz@mychirotouch.com>
2023-12-26 14:52:26 -0300

    Add favicon to layout

--- src/infra/http/www/templates/layout.ts
+++ src/infra/http/www/templates/layout.ts
@@ -16,6 +16,7 @@ export function layout({ header = null, body = null }: LayoutProps) {
         <script src="/dist/bundle.js"></script>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
         <link rel="stylesheet" href="/dist/tailwind.css">
+        <link rel="icon" type="image/x-icon" href="/assets/flare.svg">
         <title>Light Beam News</title>
       </head>
       <body class="bg-gray-100 flex flex-col">
