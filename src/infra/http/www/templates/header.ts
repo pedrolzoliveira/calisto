d2421bb6465590a3b902bf66261a235aeafc678b
Pedro Oliveira <pluiz@mychirotouch.com>
2023-12-26 18:33:25 -0300

    refactor header

--- src/infra/http/www/templates/header.ts
+++ src/infra/http/www/templates/header.ts
@@ -12,12 +12,23 @@ export interface HeaderProps {
 export function header(props?: HeaderProps) {
   return html`
     <header class="h-12 px-2 bg-white sticky top-0 border-b flex justify-between items-center">
-      <div class="space-x-2 flex items-center">
+      <div class="flex-1 flex justify-start">
         <img class="h-8" src="/assets/logo.svg" alt="logo"/>
-        <a href="/news">Notícias</a>
-        <a href="/profiles">Gerenciar Perfis</a>
-        ${props ? profilesSelect(props) : null}
       </div>
-      <a href="/users/sign-out">Sair</a>
+      ${props ? profilesSelect(props) : null}
+      <div class="flex space-x-2 flex-1 justify-end">
+        <a href="/news" class="flex items-center space-x-2 hover:bg-gray-50 px-4 py-2 rounded">
+          <span class="material-symbols-outlined">newspaper</span>
+          <p class="text-sm">Notícias</p>
+        </a>
+        <a href="/profiles" class="flex items-center space-x-2 hover:bg-gray-50 px-4 py-2 rounded">
+          <span class="material-symbols-outlined">manage_accounts</span>
+          <p class="text-sm">Gerenciar Perfis</p>
+        </a>
+        <a href="/users/sign-out" class="flex items-center space-x-2 hover:bg-gray-50 px-4 py-2 rounded">
+          <span class="material-symbols-outlined">logout</span>
+          <p class="text-sm">Sair</p>
+        </a>
+      </div>
     </header>`;
 }
