44500182917bb0d5c7f6ea588628aa7120d5a2cf
Pedro Oliveira <pluiz@mychirotouch.com>
2024-03-13 23:22:47 -0300

    Delete unused news analyser pages

--- src/infra/http/www/templates/pages/news-analyser.ts
+++
@@ -1,6 +0,0 @@
-import { html } from '@lit-labs/ssr';
-import { type NewsAnalyserProps } from '../../web-components/news-analyser';
-
-export function newsAnalyserPage(data: NewsAnalyserProps) {
-  return html`<news-analyser data=${JSON.stringify(data)}></news-analyser>`;
-}
