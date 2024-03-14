d93e4fec5f9948768695a23e46a5006b4da6cba0
Pedro Oliveira <pluiz@mychirotouch.com>
2024-03-13 23:31:53 -0300

    Remove commented out code in client.ts

--- src/application/chat-gpt/client.ts
+++ src/application/chat-gpt/client.ts
@@ -2,7 +2,6 @@ import { env } from '@/src/config/env';
 import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
-  // organization: env.CHAT_GPT_ORGANIZATION_ID,
   apiKey: env.CHAT_GPT_KEY
 });
 
