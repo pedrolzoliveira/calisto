d576bb9da0b898b1493951508f5cef9aabe9713a
Pedro Oliveira <pluiz@mychirotouch.com>
2024-03-21 21:56:44 -0300

    Add seed data for sources and source icons

---
+++ src/infra/database/prisma/migrations/20240321234923_seed_sources/migration.sql
@@ -0,0 +1,7 @@
+INSERT INTO "Source" ("code", "name", "avatarUrl")
+VALUES
+  ('estadao', 'Estadão'           , '/assets/source-icons/estadao.jpeg'),
+  ('folha'  , 'Folha de São Paulo', '/assets/source-icons/folha.jpeg'),
+  ('valor'  , 'Valor'             , '/assets/source-icons/valor.jpeg'),
+  ('uol'    , 'Uol'               , '/assets/source-icons/uol.jpeg'),
+  ('g1'     , 'G1'                , '/assets/source-icons/g1.jpeg');
