bcda7470404e48e30f8b4368858fcc20ff2ce5f2
Pedro Oliveira <pluiz@mychirotouch.com>
2024-03-23 18:44:52 -0300

    refactor folhaScraper

--- src/application/news/scrapers/folha-scraper.ts
+++ src/application/news/scrapers/folha-scraper.ts
@@ -4,16 +4,11 @@ import { getHTML } from '../utils/get-html';
 export const folhaScraper = new Scraper({
   sourceCode: 'folha',
   getLinks: async () => {
-    const links = [];
     const html = await getHTML('https://www1.folha.uol.com.br/news-sitemap.xml');
 
-    const linkElements = html.querySelectorAll('url > loc');
-    for (let i = 0; i < 20; i++) {
-      const link = linkElements[i].text;
-      links.push(link);
-    }
-
-    return links;
+    return Array.from(
+      html.querySelectorAll('url > loc')
+    ).slice(0, 20).map(({ text }) => text);
   },
   getContent: html => [
     html.querySelector('.c-content-head__title')?.text.trim(),
