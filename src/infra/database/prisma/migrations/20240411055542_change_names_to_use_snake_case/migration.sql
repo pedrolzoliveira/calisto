245e86e7f2b7525296511f80c8ee2dd330cd2973
Pedro Oliveira <pluiz@mychirotouch.com>
2024-04-11 02:57:01 -0300

    Refactor database schema and migration scripts to use snake_case naming convention

---
+++ src/infra/database/prisma/migrations/20240411055542_change_names_to_use_snake_case/migration.sql
@@ -0,0 +1,41 @@
+ALTER TABLE "CategoryEmbedding" RENAME TO "category_embeddings";
+ALTER TABLE "Source" RENAME TO "sources";
+ALTER TABLE "News" RENAME TO "news";
+ALTER TABLE "NewsEmbedding" RENAME TO "news_embeddings";
+ALTER TABLE "User" RENAME TO "users";
+ALTER TABLE "Profile" RENAME TO "profiles";
+ALTER TABLE "UserPassword" RENAME TO "user_passwords";
+ALTER TABLE "ResetPasswordToken" RENAME TO "reset_password_tokens";
+ALTER TABLE "Queue" RENAME TO "queues";
+
+ALTER TABLE "news" RENAME COLUMN "createdAt" TO "created_at";
+ALTER TABLE "news_embeddings" RENAME COLUMN "newsSectionIndex" TO "news_section_index";
+ALTER TABLE "queues" RENAME COLUMN "createdAt" TO "created_at";
+ALTER TABLE "profiles" RENAME COLUMN "createdAt" TO "created_at";
+ALTER TABLE "reset_password_tokens" RENAME COLUMN "createdAt" TO "created_at";
+ALTER TABLE "reset_password_tokens" RENAME COLUMN "expiresAt" TO "expires_at";
+ALTER TABLE "profiles" RENAME COLUMN "userId" TO "user_id";
+ALTER TABLE "user_passwords" RENAME COLUMN "userId" TO "user_id";
+ALTER TABLE "sources" RENAME COLUMN "avatarUrl" TO "avatar_url";
+ALTER TABLE "news" RENAME COLUMN "sourceCode" TO "source_code";
+ALTER TABLE "news" RENAME COLUMN "imageUrl" TO "image_url";
+
+ALTER INDEX "CategoryEmbedding_embedding_idx" RENAME TO "category_embeddings_embedding_idx";
+ALTER INDEX "CategoryEmbedding_pkey" RENAME TO "category_embeddings_pkey";
+ALTER INDEX "News_createdAt_idx" RENAME TO "news_created_at_idx";
+ALTER INDEX "News_pkey" RENAME TO "news_pkey";
+ALTER INDEX "NewsEmbedding_embedding_idx" RENAME TO "news_embeddings_embedding_idx";
+ALTER INDEX "NewsEmbedding_pkey" RENAME TO "news_embeddings_pkey";
+ALTER INDEX "Profile_pkey" RENAME TO "profiles_pkey";
+ALTER INDEX "Profile_userId_name_key" RENAME TO "profiles_user_id_name_key";
+ALTER INDEX "Queue_createdAt_idx" RENAME TO "queues_created_at_idx";
+ALTER INDEX "Queue_key_idx" RENAME TO "queues_key_idx";
+ALTER INDEX "Queue_pkey" RENAME TO "queues_pkey";
+ALTER INDEX "ResetPasswordToken_pkey" RENAME TO "reset_password_token_pkey";
+ALTER INDEX "Source_pkey" RENAME TO "sources_pkey";
+ALTER INDEX "User_email_idx" RENAME TO "users_email_idx";
+ALTER INDEX "User_email_key" RENAME TO "users_email_key";
+ALTER INDEX "User_pkey" RENAME TO "users_pkey";
+ALTER INDEX "UserPassword_pkey" RENAME TO "user_passwords_pkey";
+
+ALTER TYPE "userRole" RENAME TO "user_role";
