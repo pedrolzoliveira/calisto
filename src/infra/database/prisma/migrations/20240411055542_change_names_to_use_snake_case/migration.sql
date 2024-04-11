694e3257d78e229dc81783ebfa8345070e70a296
Pedro Oliveira <pluiz@mychirotouch.com>
2024-04-11 19:07:38 -0300

    Rename foreign keys

--- src/infra/database/prisma/migrations/20240411055542_change_names_to_use_snake_case/migration.sql
+++ src/infra/database/prisma/migrations/20240411055542_change_names_to_use_snake_case/migration.sql
@@ -1,3 +1,4 @@
+-- RenameTable
 ALTER TABLE "CategoryEmbedding" RENAME TO "category_embeddings";
 ALTER TABLE "Source" RENAME TO "sources";
 ALTER TABLE "News" RENAME TO "news";
@@ -8,6 +9,7 @@ ALTER TABLE "UserPassword" RENAME TO "user_passwords";
 ALTER TABLE "ResetPasswordToken" RENAME TO "reset_password_tokens";
 ALTER TABLE "Queue" RENAME TO "queues";
 
+-- RenameColumn
 ALTER TABLE "news" RENAME COLUMN "createdAt" TO "created_at";
 ALTER TABLE "news_embeddings" RENAME COLUMN "newsSectionIndex" TO "news_section_index";
 ALTER TABLE "queues" RENAME COLUMN "createdAt" TO "created_at";
@@ -20,6 +22,7 @@ ALTER TABLE "sources" RENAME COLUMN "avatarUrl" TO "avatar_url";
 ALTER TABLE "news" RENAME COLUMN "sourceCode" TO "source_code";
 ALTER TABLE "news" RENAME COLUMN "imageUrl" TO "image_url";
 
+-- RenameIndex
 ALTER INDEX "CategoryEmbedding_embedding_idx" RENAME TO "category_embeddings_embedding_idx";
 ALTER INDEX "CategoryEmbedding_pkey" RENAME TO "category_embeddings_pkey";
 ALTER INDEX "News_createdAt_idx" RENAME TO "news_created_at_idx";
@@ -31,11 +34,20 @@ ALTER INDEX "Profile_userId_name_key" RENAME TO "profiles_user_id_name_key";
 ALTER INDEX "Queue_createdAt_idx" RENAME TO "queues_created_at_idx";
 ALTER INDEX "Queue_key_idx" RENAME TO "queues_key_idx";
 ALTER INDEX "Queue_pkey" RENAME TO "queues_pkey";
-ALTER INDEX "ResetPasswordToken_pkey" RENAME TO "reset_password_token_pkey";
+ALTER INDEX "ResetPasswordToken_pkey" RENAME TO "reset_password_tokens_pkey";
 ALTER INDEX "Source_pkey" RENAME TO "sources_pkey";
 ALTER INDEX "User_email_idx" RENAME TO "users_email_idx";
 ALTER INDEX "User_email_key" RENAME TO "users_email_key";
 ALTER INDEX "User_pkey" RENAME TO "users_pkey";
 ALTER INDEX "UserPassword_pkey" RENAME TO "user_passwords_pkey";
 
+-- RenameType
 ALTER TYPE "userRole" RENAME TO "user_role";
+
+-- RenameForeignKey
+ALTER TABLE "news" RENAME CONSTRAINT "News_sourceCode_fkey" TO "news_source_code_fkey";
+ALTER TABLE "news_embeddings" RENAME CONSTRAINT "NewsEmbedding_link_fkey" TO "news_embeddings_link_fkey";
+ALTER TABLE "profiles" RENAME CONSTRAINT "Profile_userId_fkey" TO "profiles_user_id_fkey";
+ALTER TABLE "reset_password_tokens" RENAME CONSTRAINT "ResetPasswordToken_email_fkey" TO "reset_password_tokens_email_fkey";
+ALTER TABLE "user_passwords" RENAME CONSTRAINT "UserPassword_userId_fkey" TO "user_passwords_user_id_fkey";
+
