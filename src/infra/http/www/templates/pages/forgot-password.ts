1c372cc4af9f9fac251ac9cf66d695e0d5602d75
Pedro Oliveira <pluiz@mychirotouch.com>
2024-04-13 00:44:49 -0300

    Update form layouts and fix layout shifts

--- src/infra/http/www/templates/pages/forgot-password.ts
+++ src/infra/http/www/templates/pages/forgot-password.ts
@@ -5,7 +5,7 @@ import { button } from '../components/button';
 export function forgotPassword() {
   return html`
     <main class="flex justify-center items-center p-4 h-screen">
-      <form hx-post="/users/forgot-password" class="flex flex-col space-y-4 w-96">
+      <form hx-post="/users/forgot-password" class="flex flex-col space-y-4 w-full sm:w-96">
         <img class="pb-12" src="/assets/logo.svg" alt="logo"/>
         <h1 class="font-bold text-lg">Redefinir senha</h1>
         <div class="flex flex-col">
