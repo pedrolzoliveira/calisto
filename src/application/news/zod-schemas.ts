65f1fdc9b0f94a344d2a1b74b07ae0a0b73f650e
Pedro Oliveira <pluiz@mychirotouch.com>
2024-04-13 11:20:36 -0300

    Update default limit value to 50 in getNewsRequestSchema

--- src/application/news/zod-schemas.ts
+++ src/application/news/zod-schemas.ts
@@ -2,6 +2,6 @@ import { z } from 'zod';
 
 export const getNewsRequestSchema = z.object({
   profileId: z.string().uuid(),
-  limit: z.number({ coerce: true }).default(20),
+  limit: z.number({ coerce: true }).default(50),
   cursor: z.date({ coerce: true }).default(new Date())
 });
