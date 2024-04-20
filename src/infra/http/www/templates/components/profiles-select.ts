a0705d7cf8f63e821735f83ad149c1bac8378b6f
Pedro Oliveira <48186206+pedrolzoliveira@users.noreply.github.com>
2024-04-20 01:22:29 -0300

    Upgrade header (#36)
    * create header component
    
    * Update header component to add mobile menu functionality
    
    * Update profiles-select.ts to add spinner in main when selecting a profile
    
    * delete light-beam-news-header web-element
    
    ---------
    
    Co-authored-by: Pedro Oliveira <pluiz@mychirotouch.com>

--- src/infra/http/www/templates/components/profiles-select.ts
+++ src/infra/http/www/templates/components/profiles-select.ts
@@ -20,10 +20,19 @@ export function profilesSelect({ profileId, profiles }: ProfileSelectProps) {
   });
 
   return html`
+    <script>
+      function setSpinnerInMain() {
+        const main = document.querySelector('main');
+        if (main) {
+          main.innerHTML = '<div></div><span id="spinner" class="material-symbols-outlined animate-spin">progress_activity</span>';
+        }
+      }
+    </script>
     <div class="flex justify-center items-center px-2 space-x-2 flex-1">
-      <label class="text-sm" for="select-profiles">Perfil: </label>
-      <select name="profileId" id="select-profiles" class="p-2 border rounded" hx-get="/news/fetch?${searchParams.toString()}" hx-trigger="change" hx-swap="innerHTML" hx-target="main">
+      <label class="text-sm hidden sm:block" for="select-profiles">Perfil: </label>
+      <select onchange="setSpinnerInMain()" name="profileId" id="select-profiles" class="w-full max-w-96 p-2 border rounded" hx-get="/news/fetch?${searchParams.toString()}" hx-trigger="change" hx-swap="innerHTML" hx-target="main">
         ${map(profiles, profile => html`<option value="${profile.id}" ?selected=${profile.id === profileId}>${profile.name}</option>`)}
       </select>
-    </div>`;
+    </div>
+    `;
 }
--- src/infra/http/www/templates/header.ts
+++ src/infra/http/www/templates/header.ts
@@ -15,7 +15,7 @@ export interface HeaderProps {
 export function header(props?: HeaderProps) {
   return html`
     <header class="h-12 px-2 bg-white sticky top-0 border-b flex justify-between items-center">
-      <div class="flex-1 flex justify-start">
+      <div class="lg:flex-1 flex justify-start">
         <img class="h-8" src="/assets/logo.svg" alt="logo"/>
       </div>
       ${
@@ -23,19 +23,63 @@ export function header(props?: HeaderProps) {
         ? profilesSelect(props.profilesData)
         : nothing
       }
-      <div class="flex space-x-2 flex-1 justify-end">
-        <a href="/news" class="flex items-center space-x-2 hover:bg-gray-50 px-4 py-2 rounded">
+      <div class="flex space-x-2 sm:flex-1 justify-end">
+        <a href="/news" class="hidden sm:flex items-center space-x-2 hover:bg-gray-50 px-4 py-2 rounded">
           <span class="material-symbols-outlined">newspaper</span>
           <p class="text-sm">Notícias</p>
         </a>
-        <a href="/profiles" class="flex items-center space-x-2 hover:bg-gray-50 px-4 py-2 rounded">
+        <a href="/profiles" class="hidden sm:flex items-center space-x-2 hover:bg-gray-50 px-4 py-2 rounded">
           <span class="material-symbols-outlined">manage_accounts</span>
           <p class="text-sm">Gerenciar Perfis</p>
         </a>
-        <a href="/users/sign-out" class="flex items-center space-x-2 hover:bg-gray-50 px-4 py-2 rounded">
+        <a href="/users/sign-out" class="hidden sm:flex items-center space-x-2 hover:bg-gray-50 px-4 py-2 rounded">
           <span class="material-symbols-outlined">logout</span>
           <p class="text-sm">Sair</p>
         </a>
+        <div class="relative sm:hidden">
+            <button id="menu-btn" class="flex items-center space-x-2 hover:bg-gray-50 px-4 py-2 rounded sm:hidden">
+              <span class="material-symbols-outlined">menu</span>
+            </button>
+            <dialog id="menu-dialog" class="bg-white rounded shadow absolute right-0 m-0 sm:hidden -translate-x-[65%] w-44">
+              <a href="/news" class="flex items-center space-x-2 hover:bg-gray-50 px-4 py-3 rounded">
+                <span class="material-symbols-outlined">newspaper</span>
+                <p class="text-sm">Notícias</p>
+              </a>
+              <a href="/profiles" class="flex items-center space-x-2 hover:bg-gray-50 px-4 py-3 rounded">
+                <span class="material-symbols-outlined">manage_accounts</span>
+                <p class="text-sm">Gerenciar Perfis</p>
+              </a>
+              <a href="/users/sign-out" class="flex items-center space-x-2 hover:bg-gray-50 px-4 py-3 rounded">
+                <span class="material-symbols-outlined">logout</span>
+                <p class="text-sm">Sair</p>
+              </a>
+            </dialog>
+          </div>
       </div>
-    </header>`;
+    </header>
+    <script>
+      const menuBtn = document.getElementById('menu-btn');
+      const menuDialog = document.getElementById('menu-dialog');
+      
+      if (menuBtn && !menuBtn.hasAttribute('data-event-click-attached')) {
+        menuBtn.addEventListener('click', (event) => {
+          const wasOpen = menuDialog.open;
+          menuDialog.show();
+
+          if (!wasOpen) {
+            event.stopPropagation();
+          }
+        });
+        menuBtn.setAttribute('data-event-click-attached', '');
+      }
+
+      if (!document.body.hasAttribute('data-event-click-attached-close-menu')) {
+        document.body.addEventListener('click', (event) => {
+          if (!menuDialog.open) return;
+
+          menuDialog.close();
+        });
+        document.body.setAttribute('data-event-click-attached-close-menu', '');
+      }
+    </script>`;
 }
