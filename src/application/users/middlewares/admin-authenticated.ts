5e1c178004685b77ad80f2ff77462b55a87a6c44
Pedro Oliveira <pluiz@mychirotouch.com>
2024-04-21 01:52:37 -0300

    Update user authentication middleware to handle HX-Request header

--- src/application/users/middlewares/admin-authenticated.ts
+++ src/application/users/middlewares/admin-authenticated.ts
@@ -2,7 +2,10 @@ import { type Request, type Response, type NextFunction } from 'express';
 
 export const adminAuthenticated = (req: Request, res: Response, next: NextFunction) => {
   if (req.session.user?.role !== 'admin') {
-    return res.redirect('/users/sign-in');
+    const redirectUrl = '/users/sign-in';
+    return req.headers['hx-request']
+      ? res.setHeader('HX-Redirect', redirectUrl).end()
+      : res.redirect(redirectUrl);
   }
 
   return next();
--- src/application/users/middlewares/user-unauthenticated.ts
+++ src/application/users/middlewares/user-unauthenticated.ts
@@ -2,7 +2,10 @@ import { type Request, type Response, type NextFunction } from 'express';
 
 export const userUnauthenticated = (req: Request, res: Response, next: NextFunction) => {
   if (req.session.user) {
-    return res.redirect('/news');
+    const redirectUrl = '/news';
+    return req.headers['hx-request']
+      ? res.setHeader('HX-Redirect', redirectUrl).end()
+      : res.redirect(redirectUrl);
   }
 
   return next();
