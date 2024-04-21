c503441fb3c3be7d042387ca18b64cd4ff301e1e
Pedro Oliveira <pluiz@mychirotouch.com>
2024-04-21 18:19:37 -0300

    Add trust proxy setting for production environment in server.ts

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -10,10 +10,15 @@ import { RenderResultReadable } from '@lit-labs/ssr/lib/render-result-readable';
 import { applicationController } from '@/src/application/controller';
 import { session } from './session';
 import { asyncLocalStorage } from './async-storage';
+import { env } from '@/src/config/env';
 
 export const server = express();
 
 server.use(cookieParser());
+
+if (env.NODE_ENV === 'production') {
+  server.set('trust proxy', 1);
+}
 
 server.use(session);
 
