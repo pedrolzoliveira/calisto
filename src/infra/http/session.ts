f36a2faa4141db41b54afc8de5976f9fa35ad02e
Pedro Oliveira <pluiz@mychirotouch.com>
2024-04-21 13:59:25 -0300

    Add WEEK_MS constant and set session cookie options in session.ts

--- src/infra/http/session.ts
+++ src/infra/http/session.ts
@@ -2,6 +2,7 @@ import { env } from '@/src/config/env';
 import expressSession from 'express-session';
 import PgStoreImport from 'connect-pg-simple';
 import { pgConn } from '@/src/config/pg-conn';
+import { WEEK_MS } from '@/src/constants';
 
 const PgStore = PgStoreImport(expressSession);
 
@@ -11,5 +12,12 @@ export const session = expressSession({
   store: pgStore,
   secret: env.SESSION_SECRET,
   saveUninitialized: false,
-  resave: false
+  resave: false,
+  cookie: {
+    maxAge: WEEK_MS,
+    secure: env.NODE_ENV === 'production',
+    sameSite: 'strict',
+    httpOnly: true,
+    path: '/'
+  }
 });
